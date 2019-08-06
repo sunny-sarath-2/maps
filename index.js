7108ec4d261eaedb7cf233fa47ebd16acaece251
sunny-sarath-2 <sunnysarath2@gmail.com>
2019-08-06 19:43:38 +0530

    intial

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname + "/index.html"));
+})
+
+app.get("/submit", (req, res) => {
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',["./hello.py", 
+    req.query.lat, 
+    req.query.lng] );
+    process.stdout.on('data', function(data) { 
+        res.json(data);//.toString() 
+    } )
+// res.send({check:'check'})    
+});
+
+var port = 3000;
+app.listen(port, () => console.log("running on port ", port));
